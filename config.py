bbbcc89d5fea5cf441abc41a087a5c6b08526740
Sunwoo Lee <lsw8999@naver.com>
2022-06-06 01:52:30 +0900

    Deeplearning Updated But Dictionary should be changed

---
+++ config.py
@@ -0,0 +1,4 @@
+ELASTIC_SEARCH_URL = 'https://search-dawdling-o7og3g3rdytq5ahzfbtq3hv4v4.ap-northeast-2.es.amazonaws.com'
+ELASTIC_SEARCH_ID = 'Seonu'
+ELASTIC_SEARCH_PW = '1Q2w3e4r!'
+ELASTIC_SEARCH_AUTH = (ELASTIC_SEARCH_ID, ELASTIC_SEARCH_PW)
